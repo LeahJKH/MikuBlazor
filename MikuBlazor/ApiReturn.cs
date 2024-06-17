6e790a58bf9bb8142b900a28c2c6888a9c690c5f
LeahJKH <leahjb06.kodehode@gmail.com>
2024-06-17 09:57:10 +0200

    Add project files.

---
+++ MikuBlazor/ApiReturn.cs
@@ -0,0 +1,16 @@
+﻿namespace MikuBlazor
+{
+    public class ApiReturn
+    {
+        public string? name { get; set; }
+        public string? codename { get; set; }
+        public string? introduction { get; set; }
+        public string? version { get; set; }
+        public string? image { get; set; }
+        public static List<ApiReturn> ConvertJsonToList(string JsonString)
+        {
+            return System.Text.Json.JsonSerializer.Deserialize<List<ApiReturn>>(JsonString) ?? new List<ApiReturn>();
+        }
+    }
+}
+
---
+++ MikuBlazor/Apibuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace MikuBlazor
+{
+    public class Apibuilder
+    {
+        public string? name { get; set; }
+        public string? codename { get; set; }
+        public string? version { get; set; }
+        public string? imgLink { get; set; }
+    };
+}
